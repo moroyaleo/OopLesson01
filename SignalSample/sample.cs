1ce9cd016405ebad10cf0e338b644206dba67ea5
LeoMoroya <31087@ojs.ac.jp>
2020-06-09 13:31:21 +0900

    配列

--- SignalSample/sample.cs
+++ SignalSample/sample.cs
@@ -1,16 +1,40 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 
-class sample : Form
+class Sample : Form
 
 {
     int data;
     public static void Main()
     {
        
-        Application.Run(new sample());
+        Application.Run(new Sample());
     }
-    public sample()
+    public Sample()
     {
-        data = 6;
+
+        var scores = new int[5];
+        int num = 0;
+        //scores[0] = 10;
+        //scores[1] = 20;
+        //scores[2] = 30;
+        //scores[3] = 40;
+        //scores[4] = 50;
+        
+       
+        for (int i = 0; i < scores.Length; i++)
+        {
+            Console.WriteLine("scores["+i+"] = ");
+            scores[i] = int.Parse(Console.ReadLine());
+
+
+        }
+        for (int i = 0; i < scores.Length; i++)
+            num += scores[i];
+
+        {
+            Console.WriteLine("入力された値の合計は"+num+"です。" );
+        }
+
     }
 }
