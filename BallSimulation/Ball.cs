04dc6e8e3dcc68834f6f4c712f6df4beb4fc77e2
LeoMoroya <31087@ojs.ac.jp>
2020-06-11 16:46:04 +0900

    加速度を変数で保持

--- BallSimulation/Ball.cs
+++ BallSimulation/Ball.cs
@@ -7,11 +7,21 @@ class Ball{
     public double YPos { get; set; } //Y座標
     public Image Img { get; set; } //画像データ
 
+    private double moveX;//加速度
+    private double moveY;//加速度 
     //コンストラクタ
+
     public Ball(){
         XPos = 0;
         YPos = 0;
-        Img = Image.FromFile(@"image\soccer_ball.png");
-
+        Img = Image.FromFile(@"images\soccer_ball.png");
+        moveX = 5;
+        moveY = 5;
     }
+    public void Move()
+    {
+        XPos += moveX;
+        YPos += moveY;
+    }
+        
 }
--- BallSimulation/Stage.cs
+++ BallSimulation/Stage.cs
@@ -1,19 +1,44 @@
-﻿using System.Windows.Forms;
+﻿using System.Drawing.Text;
+using System.Windows.Forms;
 
-class Stage : Form
-{
+class Stage : Form {
+    private PictureBox pictureBoxBall = new PictureBox();
+    private Ball ball;
+
+    private Timer timer = new Timer();
     public static void Main()
     {
         Application.Run(new Stage());
 
     }
-    public Stage () {
+    public Stage() {
         Width = 1200;
         Height = 800;
         Text = "ボールシミュレーション";
 
+        ball = new Ball();
+        
+        pictureBoxBall.Width = 100;
+        pictureBoxBall.Height = 100;
+        pictureBoxBall.Top = (int)ball.YPos;
+        pictureBoxBall.Left = (int)ball.XPos;
+        pictureBoxBall.SizeMode = PictureBoxSizeMode.StretchImage;
+        pictureBoxBall.Image = ball.Img;
+        pictureBoxBall.Parent = this;
 
+        timer.Interval = 10; //ms
+        timer.Tick += Timer_Tick;
+        timer.Start();
     }
 
+        private void Timer_Tick(object sender, System.EventArgs e)
+        {
+            ball.Move();
+            pictureBoxBall.Top = (int)ball.YPos;
+            pictureBoxBall.Left = (int)ball.XPos;
+
+        }
+    }
+
+
     
-}
