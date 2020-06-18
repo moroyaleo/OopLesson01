9c2cabe8e527698fcbd53a64d5c07074188556f8
LeoMoroya <31087@ojs.ac.jp>
2020-06-18 17:33:41 +0900

    0618PrintList

--- DistanceConverter/Program.cs
+++ DistanceConverter/Program.cs
@@ -8,21 +8,61 @@ namespace DistanceConverter
 {
     class Program
     {
-      
+
         static void Main(string[] args)
+
         {
-            //フィートからメールへの対応表を出力
-            for (int feet = 1; feet < 10; feet++)
+
+            if (args.Length >= 1 && args[0] == "-tom")
+
             {
-                
-                Console.WriteLine("{0} ft = {1:0.0000} m", FeetToMeter( feet));
+
+                //フィートからメートルへの対応表を出力
+                PrintFeetToMeterList(5, 50);
+            }
+            else
+            {
+                PrintMeterToFeetList(10, 100);
+            }
+
+
+        }
+
+
+
+        static void PrintFeetToMeterList(int start, int stop)
+        {
+            for (int feet = start; feet <= stop; feet++)
+            {
+                Console.WriteLine("{0} ft = {1:0.0000} m", feet, feet * 0.3048);
 
             }
         }
-        static double FeetToMeter(int feet)
-        {
-            return (feet * 0.3048);
-        }
-    }
 
+
+
+
+
+        static void PrintMeterToFeetList(int start, int stop)
+        {
+            for (int meter = start; meter <= stop; meter++)
+            {
+
+                Console.WriteLine("{0} m = {1:0.0000} ft", meter, meter / 0.3048);
+
+            }
+
+
+
+        }
+    }    
 }
+            
+
+                
+                   
+
+                
+            
+           
+         
